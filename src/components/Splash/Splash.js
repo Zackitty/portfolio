0f06b42a8b5b4d756bb7ad2fccce9f1a63e031d6
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-08 22:02:57 -0600

    remove pixr

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -2,11 +2,9 @@ import React from 'react'
 import SideBar from '../SideBar/SideBar'
 import JiuTube from "../JiuTube/JiuTube"
 import Cause from "../Cause/Cause"
-import Pixr from '../Pixr/Pixr'
 import SurveyDonkey from "../SurveyDonkey/SurveyDonkey"
 import './Splash.css'
 function Splash() {
-  console.log("dafsd")
   return (
     <div className="splashDiv">
   <SideBar />
